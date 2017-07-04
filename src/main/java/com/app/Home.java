8aa1473d1f11c9fa4eb9b42ff4df20786b2ea1d5
Krishna Rao S <krao9@l-sb8w3eafd5-m.walmart.com>
2017-07-04 13:25:14 -0700

    Added Employee POJO

--- src/main/java/com/app/Home.java
+++ src/main/java/com/app/Home.java
@@ -1,7 +1,16 @@
 package com.app;
 
+import com.app.pojo.Employee;
+
 public class Home{
 	public static void main(String args[]){
-		System.out.println("Hello...Home...!!!!!");
+		System.out.println("Employee Details.......");
+
+		Employee employee = new Employee();
+
+		employee.setFirstName("Krishna Rao");
+		employee.setLastName("Srirambatla");
+
+		System.out.println(employee.getFirstName()+"   "+employee.getLastName());
 	}
 }
---
+++ src/main/java/com/app/pojo/Employee.java
@@ -0,0 +1,22 @@
+package com.app.pojo;
+
+public class Employee{
+	private String firstName;
+	private String lastName;
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+}
